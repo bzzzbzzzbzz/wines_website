72756907d215bfe01730673d46c73315b7cdbcb4
bzzzbzzzbzz <s.meln1k@ukr.net>
2023-09-03 11:33:23 +0200

    update

--- main.py
+++ main.py
@@ -1,78 +1,78 @@
-import datetime, pandas, pprint, collections
+import datetime, pandas, collections, argparse
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def grammar_func(year):
-    danger_years = [111, 112, 113, 114]
-    if year in danger_years:
-        return 'лет'
-    elif year % 10 == 0:
-        return 'лет'
-    elif year % 10 == 1:
-        return 'год'
-    elif year % 10 >= 5:
-        return 'лет'
-    else:
-        return 'года'
 
 
-excel_data = pandas.read_excel('wine3.xlsx',
-                               na_filter=False
-                               )
-data_dict = excel_data.to_dict()
+parsed_xlsx = argparse.ArgumentParser()
+parsed_xlsx.add_argument('xlsx', help='enter your xlsx')
+args = parsed_xlsx.parse_args()
 
-white_wine = []
-red_wine = []
+excel_data = pandas.read_excel(args.xlsx, na_filter=False)
+
+
+wines_data = excel_data.to_dict()
+
+white_wines = []
+red_wines = []
 drinks = []
 
 types = []
-for key in data_dict:
+for key in wines_data:
     types.append(key)
 
 i = 0
-for inside_key in data_dict[types[i]]:
+for inside_key in wines_data[types[i]]:
     new_dict = {}
-    for key in data_dict:
-        if not data_dict[key][inside_key]:
-            data_dict[key][inside_key] = ''
-        new_dict[key] = data_dict[key][inside_key]
-    if new_dict[types[0]] == data_dict[types[0]][0]:
-        white_wine.append(new_dict)
-    elif new_dict[types[0]] == data_dict[types[0]][3]:
-        red_wine.append(new_dict)
+    for key in wines_data:
+        new_dict[key] = wines_data[key][inside_key]
+    if new_dict[types[0]] == wines_data[types[0]][0]:
+        white_wines.append(new_dict)
+    elif new_dict[types[0]] == wines_data[types[0]][3]:
+        red_wines.append(new_dict)
     else:
         drinks.append(new_dict)
 
 final_dictionary = {}
-final_dictionary.update({data_dict[types[0]][0]: white_wine})
-final_dictionary.update({data_dict[types[0]][3]: red_wine})
-final_dictionary.update({data_dict[types[0]][1]: drinks})
+final_dictionary.update({wines_data[types[0]][0]: white_wines})
+final_dictionary.update({wines_data[types[0]][3]: red_wines})
+final_dictionary.update({wines_data[types[0]][1]: drinks})
 
-alcohol = collections.defaultdict(str, final_dictionary)
-
-
-
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
+alcohols = collections.defaultdict(str, final_dictionary)
 
 difference = datetime.datetime.today() - datetime.datetime(year=1920, month=1, day=1)
-year_var = difference.days // 365
+product_year = difference.days // 365
 
-rendered_page = template.render(
-    product_year=year_var,
-    correct_year=grammar_func(year_var),
-    alcohol=alcohol,
-)
+def is_year(year):
+    year = str(year)
+    if year[-1] in '0':
+        return 'лет'
+    elif year[-1] in '1':
+        return 'год'
+    elif year[-1] in '234':
+        return 'года'
+    else:
+        return 'лет'
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        product_year=product_year,
+        correct_year=is_year(product_year),
+        alcohols=alcohols,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
