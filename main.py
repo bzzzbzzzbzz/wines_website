4fcdd00fcb025e60b2218f3b866f32c4cd47d8b9
bzzzbzzzbzz <s.meln1k@ukr.net>
2023-10-13 13:06:42 +0200

    deleted lots of trash code, modificate loops, added new variebles to make code more understandable

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import datetime, pandas, argparse
+import datetime, pandas, argparse, pprint
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import defaultdict
 
-def year_ending(year):
+def get_year_word(year):
     if year % 10 == 1 and year % 100 != 11:
         return 'год'
     elif 2 <= year % 10 <= 4 and (year % 100 < 10 or year % 100 >= 20):
@@ -19,22 +19,26 @@ if __name__ == '__main__':
     args = parsed_xlsx.parse_args()
     excel_data = pandas.read_excel(args.xlsx, na_filter=False)
 
-    wines_data = excel_data.to_dict()
+    excel_dict = excel_data.to_dict()
+    wines_dict = defaultdict(list)
+    category_names = []
+    rows = []
 
-    wine_dict = defaultdict(list)
-    types = []
-    for key in wines_data:
-        types.append(key)
+    for row in excel_dict:
+        rows.append(row)
+    category = rows[0]
 
-    i = 0
-    for inside_key in wines_data[types[i]]:
+    for column in excel_dict[category]:
         new_dict = {}
-        for key in wines_data:
-            new_dict[key] = wines_data[key][inside_key]
-        wine_type = new_dict[types[0]]
-        wine_dict[wine_type].append(new_dict)
+        for row in rows:
+            new_dict[row] = excel_dict[row][column]
+        category_name = new_dict[category]
+        if category_name not in category_names:
+            category_names.append(category_name)
+        drink_type = new_dict[category]
+        wines_dict[drink_type].append(new_dict)
 
-    alcohols = dict(wine_dict)
+    alcohols = dict(wines_dict)
 
     difference = datetime.datetime.today() - datetime.datetime(year=1920, month=1, day=1)
     product_year = difference.days // 365
@@ -47,8 +51,10 @@ if __name__ == '__main__':
 
     rendered_page = template.render(
         product_year=product_year,
-        correct_year=year_ending(product_year),
+        correct_year=get_year_word(product_year),
         alcohols=alcohols,
+        rows=rows,
+        category_names=category_names
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
